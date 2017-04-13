3fdba02982b74c8553df1509b8f3081a6c8e4fc8
David Saffo <dsaffo@luc.edu>
2017-04-13 13:05:57 -0500

    push
    push

---
+++ js/script.js
@@ -0,0 +1,537 @@
+var voters = new Array(16);
+//voter 1
+voters[0] = new Array(3); 
+voters[0][0] = new Array(5);
+voters[0][0][0] = 4;
+voters[0][0][1] = 2;
+voters[0][0][2] = 3;
+voters[0][0][3] = 5;
+voters[0][0][4] = 1;
+voters[0][1] = new Array(5);
+voters[0][1][0] = 4;
+voters[0][1][1] = 2;
+voters[0][1][2] = 3;
+voters[0][1][3] = 5;
+voters[0][1][4] = 1;
+voters[0][2] = new Array(5);
+voters[0][2][0] = 3;
+voters[0][2][1] = 5;
+voters[0][2][2] = 2;
+voters[0][2][3] = 4;
+voters[0][2][4] = 5;
+//voter 2
+voters[1] = new Array(3); 
+voters[1][0] = new Array(5);
+voters[1][0][0] = 4;
+voters[1][0][1] = 1;
+voters[1][0][2] = 5;
+voters[1][0][3] = 2;
+voters[1][0][4] = 3;
+voters[1][1] = new Array(5);
+voters[1][1][0] = 4;
+voters[1][1][1] = 5;
+voters[1][1][2] = 3;
+voters[1][1][3] = 2;
+voters[1][1][4] = 1;
+voters[1][2] = new Array(5);
+voters[1][2][0] = 1;
+voters[1][2][1] = 2;
+voters[1][2][2] = 3;
+voters[1][2][3] = 4;
+voters[1][2][4] = 5;
+//voter 3
+voters[2] = new Array(3); 
+voters[2][0] = new Array(5);
+voters[2][0][0] = 3;
+voters[2][0][1] = 2;
+voters[2][0][2] = 4;
+voters[2][0][3] = 5;
+voters[2][0][4] = 1;
+voters[2][1] = new Array(5);
+voters[2][1][0] = 1;
+voters[2][1][1] = 2;
+voters[2][1][2] = 4;
+voters[2][1][3] = 5;
+voters[2][1][4] = 3;
+voters[2][2] = new Array(5);
+voters[2][2][0] = 1;
+voters[2][2][1] = 4;
+voters[2][2][2] = 3;
+voters[2][2][3] = 2;
+voters[2][2][4] = 5;
+//voter 4
+voters[3] = new Array(3); 
+voters[3][0] = new Array(5);
+voters[3][0][0] = 2;
+voters[3][0][1] = 3;
+voters[3][0][2] = 4;
+voters[3][0][3] = 5;
+voters[3][0][4] = 1;
+voters[3][1] = new Array(5);
+voters[3][1][0] = 1;
+voters[3][1][1] = 2;
+voters[3][1][2] = 4;
+voters[3][1][3] = 5;
+voters[3][1][4] = 3;
+voters[3][2] = new Array(5);
+voters[3][2][0] = 3;
+voters[3][2][1] = 4;
+voters[3][2][2] = 1;
+voters[3][2][3] = 5;
+voters[3][2][4] = 2;
+//voter 5
+voters[4] = new Array(3); 
+voters[4][0] = new Array(5);
+voters[4][0][0] = 2;
+voters[4][0][1] = 4;
+voters[4][0][2] = 5;
+voters[4][0][3] = 3;
+voters[4][0][4] = 1;
+voters[4][1] = new Array(5);
+voters[4][1][0] = 1;
+voters[4][1][1] = 2;
+voters[4][1][2] = 3;
+voters[4][1][3] = 4;
+voters[4][1][4] = 5;
+voters[4][2] = new Array(5);
+voters[4][2][0] = 5;
+voters[4][2][1] = 4;
+voters[4][2][2] = 2;
+voters[4][2][3] = 1;
+voters[4][2][4] = 3;
+//voter 6
+voters[5] = new Array(3); 
+voters[5][0] = new Array(5);
+voters[5][0][0] = 2;
+voters[5][0][1] = 4;
+voters[5][0][2] = 3;
+voters[5][0][3] = 4;
+voters[5][0][4] = 1;
+voters[5][1] = new Array(5);
+voters[5][1][0] = 5;
+voters[5][1][1] = 4;
+voters[5][1][2] = 3;
+voters[5][1][3] = 1;
+voters[5][1][4] = 2;
+voters[5][2] = new Array(5);
+voters[5][2][0] = 1;
+voters[5][2][1] = 3;
+voters[5][2][2] = 2;
+voters[5][2][3] = 4;
+voters[5][2][4] = 5;
+//voter 7
+voters[6] = new Array(3); 
+voters[6][0] = new Array(5);
+voters[6][0][0] = 3;
+voters[6][0][1] = 2;
+voters[6][0][2] = 5;
+voters[6][0][3] = 4;
+voters[6][0][4] = 1;
+voters[6][1] = new Array(5);
+voters[6][1][0] = 4;
+voters[6][1][1] = 2;
+voters[6][1][2] = 5;
+voters[6][1][3] = 3;
+voters[6][1][4] = 1;
+voters[6][2] = new Array(5);
+voters[6][2][0] = 2;
+voters[6][2][1] = 1;
+voters[6][2][2] = 5;
+voters[6][2][3] = 4;
+voters[6][2][4] = 3;
+//voter 8
+voters[7] = new Array(3); 
+voters[7][0] = new Array(5);
+voters[7][0][0] = 4;
+voters[7][0][1] = 2;
+voters[7][0][2] = 5;
+voters[7][0][3] = 3;
+voters[7][0][4] = 1;
+voters[7][1] = new Array(5);
+voters[7][1][0] = 5;
+voters[7][1][1] = 4;
+voters[7][1][2] = 1;
+voters[7][1][3] = 2;
+voters[7][1][4] = 3;
+voters[7][2] = new Array(5);
+voters[7][2][0] = 3;
+voters[7][2][1] = 5;
+voters[7][2][2] = 4;
+voters[7][2][3] = 2;
+voters[7][2][4] = 1;
+//voter 9
+voters[8] = new Array(3); 
+voters[8][0] = new Array(5);
+voters[8][0][0] = 2;
+voters[8][0][1] = 1;
+voters[8][0][2] = 5;
+voters[8][0][3] = 4;
+voters[8][0][4] = 3;
+voters[8][1] = new Array(5);
+voters[8][1][0] = 1;
+voters[8][1][1] = 3;
+voters[8][1][2] = 2;
+voters[8][1][3] = 5;
+voters[8][1][4] = 4;
+voters[8][2] = new Array(5);
+voters[8][2][0] = 3;
+voters[8][2][1] = 2;
+voters[8][2][2] = 4;
+voters[8][2][3] = 1;
+voters[8][2][4] = 5;
+//voter 10
+voters[9] = new Array(3); 
+voters[9][0] = new Array(5);
+voters[9][0][0] = 4;
+voters[9][0][1] = 1;
+voters[9][0][2] = 5;
+voters[9][0][3] = 3;
+voters[9][0][4] = 2;
+voters[9][1] = new Array(5);
+voters[9][1][0] = 3;
+voters[9][1][1] = 5;
+voters[9][1][2] = 4;
+voters[9][1][3] = 2;
+voters[9][1][4] = 1;
+voters[9][2] = new Array(5);
+voters[9][2][0] = 1;
+voters[9][2][1] = 3;
+voters[9][2][2] = 5;
+voters[9][2][3] = 4;
+voters[9][2][4] = 2;
+//voter 11
+voters[10] = new Array(3); 
+voters[10][0] = new Array(5);
+voters[10][0][0] = 1;
+voters[10][0][1] = 4;
+voters[10][0][2] = 3;
+voters[10][0][3] = 2;
+voters[10][0][4] = 5;
+voters[10][1] = new Array(5);
+voters[10][1][0] = 4;
+voters[10][1][1] = 3;
+voters[10][1][2] = 5;
+voters[10][1][3] = 1;
+voters[10][1][4] = 2;
+voters[10][2] = new Array(5);
+voters[10][2][0] = 3;
+voters[10][2][1] = 4;
+voters[10][2][2] = 2;
+voters[10][2][3] = 5;
+voters[10][2][4] = 1;
+//voter 12
+voters[11] = new Array(3); 
+voters[11][0] = new Array(5);
+voters[11][0][0] = 5;
+voters[11][0][1] = 3;
+voters[11][0][2] = 1;
+voters[11][0][3] = 4;
+voters[11][0][4] = 2;
+voters[11][1] = new Array(5);
+voters[11][1][0] = 4;
+voters[11][1][1] = 1;
+voters[11][1][2] = 3;
+voters[11][1][3] = 4;
+voters[11][1][4] = 5;
+voters[11][2] = new Array(5);
+voters[11][2][0] = 2;
+voters[11][2][1] = 1;
+voters[11][2][2] = 3;
+voters[11][2][3] = 5;
+voters[11][2][4] = 4;
+//voter 13
+voters[12] = new Array(3); 
+voters[12][0] = new Array(5);
+voters[12][0][0] = 2;
+voters[12][0][1] = 4;
+voters[12][0][2] = 1;
+voters[12][0][3] = 5;
+voters[12][0][4] = 3;
+voters[12][1] = new Array(5);
+voters[12][1][0] = 3;
+voters[12][1][1] = 1;
+voters[12][1][2] = 5;
+voters[12][1][3] = 4;
+voters[12][1][4] = 2;
+voters[12][2] = new Array(5);
+voters[12][2][0] = 3;
+voters[12][2][1] = 5;
+voters[12][2][2] = 1;
+voters[12][2][3] = 2;
+voters[12][2][4] = 4;
+//voter 14
+voters[13] = new Array(3); 
+voters[13][0] = new Array(5);
+voters[13][0][0] = 2;
+voters[13][0][1] = 1;
+voters[13][0][2] = 3;
+voters[13][0][3] = 4;
+voters[13][0][4] = 5;
+voters[13][1] = new Array(5);
+voters[13][1][0] = 1;
+voters[13][1][1] = 2;
+voters[13][1][2] = 4;
+voters[13][1][3] = 5;
+voters[13][1][4] = 3;
+voters[13][2] = new Array(5);
+voters[13][2][0] = 5;
+voters[13][2][1] = 3;
+voters[13][2][2] = 1;
+voters[13][2][3] = 4;
+voters[13][2][4] = 2;
+//voter 15
+voters[14] = new Array(3); 
+voters[14][0] = new Array(5);
+voters[14][0][0] = 4;
+voters[14][0][1] = 5;
+voters[14][0][2] = 2;
+voters[14][0][3] = 1;
+voters[14][0][4] = 3;
+voters[14][1] = new Array(5);
+voters[14][1][0] = 2;
+voters[14][1][1] = 1;
+voters[14][1][2] = 5;
+voters[14][1][3] = 4;
+voters[14][1][4] = 3;
+voters[14][2] = new Array(5);
+voters[14][2][0] = 1;
+voters[14][2][1] = 2;
+voters[14][2][2] = 3;
+voters[14][2][3] = 5;
+voters[14][2][4] = 4;
+//voter 16
+voters[15] = new Array(3); 
+voters[15][0] = new Array(5);
+voters[15][0][0] = 4;
+voters[15][0][1] = 1;
+voters[15][0][2] = 2;
+voters[15][0][3] = 3;
+voters[15][0][4] = 5;
+voters[15][1] = new Array(5);
+voters[15][1][0] = 4;
+voters[15][1][1] = 5;
+voters[15][1][2] = 3;
+voters[15][1][3] = 2;
+voters[15][1][4] = 1;
+voters[15][2] = new Array(5);
+voters[15][2][0] = 2;
+voters[15][2][1] = 3;
+voters[15][2][2] = 4;
+voters[15][2][3] = 5;
+voters[15][2][4] = 1;
+
+var donut = 0;
+var cake = 0;
+var cupcake = 0;
+var icecream = 0;
+var pie = 0;
+for (var i = 0; i <16 ; i++){
+    if (voters[i][0][0] == 1)
+       donut++;
+    else if (voters[i][0][0] == 2)
+        cake++;
+    else if (voters[i][0][0] == 3)
+        cupcake++;
+    else if (voters[i][0][0] == 4)
+        icecream++;
+    else if (voters[i][0][0] == 5)
+        pie++;
+}
+
+var i7 = 0;
+var i6s = 0;
+var s7 = 0;
+var pixel = 0;
+var n6p = 0;
+for (var i = 0; i <16 ; i++){
+    if (voters[i][1][0] == 1)
+       pixel++;
+    else if (voters[i][1][0] == 2)
+        i6s++;
+    else if (voters[i][1][0] == 3)
+        s7++;
+    else if (voters[i][1][0] == 4)
+        i7++;
+    else if (voters[i][1][0] == 5)
+        n6p++;
+}
+
+var bs = 0;
+var bt = 0;
+var pr = 0;
+var dk = 0;
+var nb = 0;
+for (var i = 0; i <16 ; i++){
+    if (voters[i][2][0] == 1)
+        bs++;
+    else if (voters[i][2][0] == 2)
+        bt++;
+    else if (voters[i][2][0] == 3)
+        pr++;
+    else if (voters[i][2][0] == 4)
+        dk++;
+    else if (voters[i][2][0] == 5)
+        nb++;
+}
+
+document.getElementById("can1").innerHTML= donut; 
+document.getElementById("can2").innerHTML= cake;
+document.getElementById("can3").innerHTML= icecream;
+document.getElementById("can4").innerHTML= pie;
+document.getElementById("can5").innerHTML= cupcake;
+var step = 0;
+var ballot = 0;
+function next(){
+    if (step == 0){
+        document.getElementById("label").innerHTML= "Phones"; 
+        document.getElementById("pic1").src = "assets/img/iphone7.jpg";
+        document.getElementById("pic2").src = "assets/img/6s.jpg";
+        document.getElementById("pic3").src = "assets/img/s7.png";
+        document.getElementById("pic4").src = "assets/img/6p.png";
+        document.getElementById("pic5").src = "assets/img/pixel.png";
+        document.getElementById("can1").innerHTML= i7; 
+        document.getElementById("can2").innerHTML= i6s;
+        document.getElementById("can3").innerHTML= s7;
+        document.getElementById("can4").innerHTML= n6p;
+        document.getElementById("can5").innerHTML= pixel;
+        document.getElementById("add1").innerHTML= 0; 
+        document.getElementById("add2").innerHTML= 0;
+        document.getElementById("add3").innerHTML= 0;
+        document.getElementById("add4").innerHTML= 0;
+        document.getElementById("add5").innerHTML= 0;
+        ballot = 1;
+        step++;
+    }
+    else{
+        document.getElementById("label").innerHTML="Algorithms";
+        document.getElementById("pic1").src = "assets/img/Dijk.png";
+        document.getElementById("pic2").src = "assets/img/bubblesort.jpg";
+        document.getElementById("pic3").src = "assets/img/pagerank.png";
+        document.getElementById("pic4").src = "assets/img/tree.png";
+        document.getElementById("pic5").src = "assets/img/naive.png";
+        document.getElementById("can1").innerHTML= dk; 
+        document.getElementById("can2").innerHTML= bs;
+        document.getElementById("can3").innerHTML= pr;
+        document.getElementById("can4").innerHTML= bt;
+        document.getElementById("can5").innerHTML= nb;
+         document.getElementById("add1").innerHTML= 0; 
+        document.getElementById("add2").innerHTML= 0;
+        document.getElementById("add3").innerHTML= 0;
+        document.getElementById("add4").innerHTML= 0;
+        document.getElementById("add5").innerHTML= 0;
+        ballot = 2;
+    }
+        
+}
+
+var stage = 0;
+var stage1 =0;
+var stage2 =0;
+function irv(){
+    if(ballot == 0){
+       if (stage == 0){
+           document.getElementById("stepText").innerHTML= "No dessert has the majority, elminate the desserts with the lowest votes";
+           document.getElementById("can1").innerHTML= "elm"; 
+           document.getElementById("can4").innerHTML= "elm";
+           stage++;
+        }
+        else if (stage == 1){
+             document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence."
+            document.getElementById("can5").innerHTML= cupcake += 1; 
+           document.getElementById("can3").innerHTML= icecream += 1;
+            document.getElementById("add5").innerHTML= "+1"; 
+           document.getElementById("add3").innerHTML= "+1";
+            stage++;
+        }
+        else if(stage == 2){
+            document.getElementById("stepText").innerHTML= "No dessert has the majority, elminate the desserts with the lowest votes";
+            document.getElementById("can5").innerHTML= "elm";
+            stage++;
+        }
+        else if(stage == 3){
+            document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence.";
+            document.getElementById("can2").innerHTML= cake += 3;
+            document.getElementById("add2").innerHTML= "+3";
+            stage++;
+        }
+        else if (stage == 4){
+             document.getElementById("stepText").innerHTML= "Cake Wins!";
+        }
+    }
+    if(ballot == 1){
+         if (stage1 == 0){
+           document.getElementById("stepText").innerHTML= "No phone has the majority, elminate the phone with the lowest votes";
+           document.getElementById("can2").innerHTML= "elm"; 
+           stage1++;
+        }
+         else if (stage1 == 1){
+             document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence."
+            document.getElementById("can1").innerHTML= i7 += 1; 
+           document.getElementById("add1").innerHTML= "+1"; 
+            stage1++;
+        }
+         else if (stage1 == 2){
+            document.getElementById("stepText").innerHTML= "No phone has the majority, elminate the phone with the lowest votes";
+            document.getElementById("can3").innerHTML= "elm"; 
+            document.getElementById("can4").innerHTML= "elm"; 
+            stage1++;
+        }
+         else if (stage1 == 3){
+             document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence."
+         
+             document.getElementById("can5").innerHTML= pixel += 4; 
+            
+             document.getElementById("add5").innerHTML= "+4"; 
+            stage1++;
+        }
+         else if (stage1 == 4){
+             document.getElementById("stepText").innerHTML= "Pixel Wins!";
+        }
+    }
+     if(ballot == 2){
+         if (stage2 == 0){
+           document.getElementById("stepText").innerHTML= "No phone has the majority, elminate the phone with the lowest votes";
+           document.getElementById("can1").innerHTML= "elm"; 
+           stage2++;
+        }
+         else if (stage2 == 1){
+             document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence."
+            
+            stage2++;
+        }
+         else if (stage2 == 2){
+            document.getElementById("stepText").innerHTML= "No phone has the majority, elminate the phone with the lowest votes";
+            document.getElementById("can5").innerHTML= "elm"; 
+            
+            stage2++;
+        }
+         else if (stage2 == 3){
+             document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence."
+         
+             document.getElementById("can4").innerHTML= bt += 1; 
+            document.getElementById("can3").innerHTML= pr += 1;
+             document.getElementById("add4").innerHTML= "+1";
+             document.getElementById("add3").innerHTML= "+1";
+            stage2++;
+        }
+         else if (stage2 == 4){
+             document.getElementById("stepText").innerHTML= "No phone has the majority, elminate the phone with the lowest votes";
+            document.getElementById("can4").innerHTML= "elm"; 
+            
+            stage2++;
+        }
+         else if (stage2 == 5){
+             document.getElementById("stepText").innerHTML= "Redistribute votes to the voters next available prefrence."
+         
+             document.getElementById("can2").innerHTML= bs += 3; 
+            document.getElementById("can3").innerHTML= pr += 1;
+             document.getElementById("add2").innerHTML= "+3";
+             document.getElementById("add3").innerHTML= "+2";
+            stage2++;
+        }
+        else if (stage2 == 6){
+             document.getElementById("stepText").innerHTML= "There is a tie, Winner is the algorithm with the most votes in the previous round. Page Rank Wins!"
+        }
+    }
+}
+
